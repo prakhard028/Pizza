ce09d08d2fb9f0bab126b3db6b769f6be9ac9b85
prakhard028 <prakhardixit28@gmail.com>
2020-08-30 12:43:21 +0530

    frontend

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const expressLayouts= require('express-ejs-layouts');
 app.use(express.static('public'));
 
 
-app.get('/',(req, res)=>{
-   res.render('home');// it will render what is shown in home
-});
+
 // template engine
 app.use(expressLayouts);
 app.set('views',path.join(__dirname,'/resources/views'));// we are telling the path from it should render
 app.set('view engine', 'ejs');// name of  the view engine
+
+
+app.get('/',(req, res)=>{
+    res.render('home');// it will render what is shown in home
+ });
+ app.get('/cart',(req, res)=>{
+     res.render('customer/cart');// it will render what is shown in home
+  });
+  app.get('/login',(req, res)=>{
+    res.render('auth/login');// it will render what is shown in home
+ });
+ app.get('/register',(req, res)=>{
+    res.render('auth/register');// it will render what is shown in home
+ });
 
 
 const PORT= process.env.PORT || 3000;
